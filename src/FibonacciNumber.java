1ecec2aa0e52caefedcd61dc2eae01d07d588bc9
Danilova Vlada <just.you177@gmail.com>
2021-02-13 11:44:59 +0300

    Initial commit

---
+++ src/FibonacciNumber.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+/**
+ * @author vladadanilova
+ */
+
+public class FibonacciNumber {
+    private static long calc_fib(int n) {
+        int[] array = new int[n+1];
+        array[0] = 0;
+        array[1] = 1;
+        for (int i = 2; i <= n; i++)
+        {
+            array[i] = array[i - 1] + array[i - 2];
+        }
+        return array[n];
+    }
+
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        System.out.println(calc_fib(n));
+    }
+}
