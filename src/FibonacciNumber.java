1c42640c1c660d9eb86a1c1bc1a1330fd2340395
Danilova Vlada <just.you177@gmail.com>
2021-02-14 23:09:29 +0300

    getFibonacciLastDigit is right now

--- src/FibonacciNumber.java
+++ src/FibonacciNumber.java
@@ -8,12 +8,17 @@ public class FibonacciNumber {
     private static long calc_fib(int n) {
         long[] array = new long[n+1];
         array[0] = 0;
-        array[1] = 1;
-        for (int i = 2; i <= n; i++)
+        if (n == 0)
         {
-            array[i] = array[i - 1] + array[i - 2];
+            return array[n];
         }
-        return array[n];
+        else {
+            array[1] = 1;
+            for (int i = 2; i <= n; i++) {
+                array[i] = array[i - 1] + array[i - 2];
+            }
+            return array[n];
+        }
     }
 
     public static void main(String args[]) {
